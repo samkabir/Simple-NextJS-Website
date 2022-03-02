fec703926c3cd091785d0943a8e52ea2272422f3
SamiulKabir <samkabir26@gmail.com>
2022-03-02 15:25:29 +0600

    made the cars look like cards

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import styles from '../../styles/Cars.module.css';
 
 const Cars = () => {
       const { register, handleSubmit } = useForm();
@@ -22,10 +23,10 @@ const Cars = () => {
       console.log(allcars);
       return (
             <div>
-                  <h1>All the Cars</h1>
+                  <h1>Select a car maker</h1>
                   <div>
                         <form onChange={handleSubmit(handleInfo)}>
-                              <select placeholder="Car Maker Name"  {...register("carmakername")}>
+                              <select className={styles.selectbutton} placeholder="Car Maker Name"  {...register("carmakername")}>
                                     <option value="toyota">Toyota</option>
                                     <option value="nissan">Nissan</option>
                                     <option value="honda">Honda</option>
@@ -33,15 +34,24 @@ const Cars = () => {
                               </select>
                         </form>
                   </div>
-                  <div>
+                  <br />
+                  <div className={styles.allcards}>
                         {isData? allcars.results.map( car => (
-                              <div key={car.car_id}>
-                                    <h2>{car.car_manufacturer.maker_name}</h2>
+                              <div>
+                                    <div key={car.car_id} className={styles.card}>
+                                          <img src={car.images.image_url} width="300px" height="200px" className={styles.image} />
+                                          <h2>{car.car_manufacturer.maker_name}</h2>
+                                          <h3>Car Model: {car.model_name.model_name}</h3>
+                                          <h6>Price: {car.affiliated_price}</h6>
+                                          <h6>Car Year: {car.car_year}</h6>
+                                          <h6>Merchant Company: {car.author}</h6>
+                                    </div>
                               </div>
-                        )) : "No value"}
+                        )) : "No Car Selected Yet"}
                   </div>
             </div>
       );
 };
+
 
 export default Cars;
